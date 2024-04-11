506ef835d95c9f2ebb3f35ea25495afb10fc4303
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2024-04-11 15:05:49 +0530

    Custom Hooks Fetch Seo Resposiveness And RTK API Color

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { colorApi } from '../features/api/colors';
 
 export const store = configureStore({
   reducer: {
+    [colorApi.reducerPath]: colorApi.reducer,
   },
+
+  // middleware: for caching, error handling, etc.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(colorApi.middleware),
 });
